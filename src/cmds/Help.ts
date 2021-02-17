1be3c484295b3ccf44e8e696720587d37ede70d9
u9g <43508353+u9g@users.noreply.github.com>
2021-02-16 23:22:51 -0500

    promisify

--- src/cmds/Help.ts
+++ src/cmds/Help.ts
@@ -1,46 +1,49 @@
+import { resolve } from "path";
 import { Command } from "../Command";
-import { Callback, CommandManager } from "../CommandManager"
+import { CommandManager } from "../CommandManager"
 
 export default function(cmdManager: CommandManager) {
 
-    function helpCommand(sender: string, _flags: any, args: string[], cb: Callback) {
-        const log = (msg: string) => cmdManager.log(sender, msg);
+    async function helpCommand(sender: string, _flags: any, args: string[]): Promise<void> {
+     return new Promise((resolve, reject) => {
+            const log = (msg: string) => cmdManager.log(sender, msg);
 
-        function listHelpFor(cmd: Command) {
-            log(`${cmd.name} - ${cmd.help}`);
-            log(`  Usage: ${cmd.usage}`)
+            function listHelpFor(cmd: Command) {
+                log(`${cmd.name} - ${cmd.help}`);
+                log(`  Usage: ${cmd.usage}`)
 
-            if (cmd.flags.length > 0) {
-                log(`  Flags:`)
-                for (const flag of cmd.flags) {
-                    log(`  ${flag.usage}`);
+                if (cmd.flags.length > 0) {
+                    log(`  Flags:`)
+                    for (const flag of cmd.flags) {
+                        log(`  ${flag.usage}`);
+                    }
                 }
             }
-        }
 
-        function listAllCommands() {
-            log('Available Commands:');
-            for (const cmd of cmdManager.commands) {
-                listHelpFor(cmd);
+            function listAllCommands() {
+                log('Available Commands:');
+                for (const cmd of cmdManager.commands) {
+                    listHelpFor(cmd);
+                }
             }
-        }
-    
-        if (args.length === 0) {
-            listAllCommands();
-        } else if (args.length === 1) {
-            const cmdName = args[0].toLowerCase();
-            const cmd = cmdManager.commands.find(c => c.name.toLowerCase() === cmdName);
+        
+            if (args.length === 0) {
+                listAllCommands();
+            } else if (args.length === 1) {
+                const cmdName = args[0].toLowerCase();
+                const cmd = cmdManager.commands.find(c => c.name.toLowerCase() === cmdName);
 
-            if (cmd == null) {
-                log(`Command '${args[0]}' not found!`);
+                if (cmd == null) {
+                    log(`Command '${args[0]}' not found!`);
+                } else {
+                    listHelpFor(cmd);
+                }
             } else {
-                listHelpFor(cmd);
+                log('Usage: help [command name]');
             }
-        } else {
-            log('Usage: help [command name]');
-        }
 
-        cb();
+            resolve()
+        })
     }
 
     cmdManager.registerCommand('help', helpCommand,
